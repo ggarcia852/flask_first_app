40a99cf7e3134592202de7696a153a6fd4188622
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2021-10-11 16:52:54 -0600

    first commit

---
+++ app/routes.py
@@ -0,0 +1,21 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    title = "My First App"
+    user = "George"
+    return render_template('index.html', user=user, title=title)
+
+
+@app.route('/favs')
+def favs():
+    title= 'Five Favorite Countries'
+    countries= ['Thailand', 'Spain', 'India', 'New Zealand', 'Nepal']
+    return render_template('favs.html', title=title, countries=countries)
+
+@app.route('/todo')
+def todo():
+    title= 'Countries to Visit'
+    countries= ['Brazil', 'South Africa', 'Japan', 'Greece', 'Peru']
+    return render_template('todo.html', title=title, countries=countries)
