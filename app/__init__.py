84935261698fc7644e5415d4af4996cc83e16c5d
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2021-10-13 11:09:23 -0600

    in class

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_migrate import Migrate, migrate
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from re import U
 from app import app
-from flask import render_template, url_for
-from app.forms import UserInfoForm
+from flask import render_template, url_for, redirect, flash
+from app.forms import UserInfoForm,  PostForm
+from app.models import Post, User
+from app import db
 
 @app.route('/')
 def index():
@@ -27,9 +28,36 @@ def todo():
 def register():
     register_form = UserInfoForm()
     if register_form.validate_on_submit():
-        print("Form submitted correctly")
         username = register_form.username.data
         email = register_form.email.data
         password = register_form.password.data
-        print(username, email, password)
+
+        existing_user = User.query.filter_by(username=username).all()
+        if existing_user:
+            flash(f'The username {username} is already in use. Please try again.', 'danger')
+            return redirect(url_for('register'))
+
+        new_user = User(username, email, password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash(f'Thank you {username}, you have successfully registered!', 'success')
+        return redirect(url_for('index'))
+
     return render_template('register.html', form=register_form)
+
+
+@app.route('/createpost', methods=['GET', 'POST'])
+def createpost():
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        content = form.content.data
+
+        new_post = Post(title, content, user_id=1)
+        db.session.add(new_post)
+        db.session.commit()
+
+        
+        
+    return render_template('createpost.html', form=form)
