4f2ae957abf50f2f81949cd39f57039a5b6df1be
George Garcia <70930059+ggarcia852@users.noreply.github.com>
2021-10-27 18:25:40 -0600

    api homework

--- app/models.py
+++ app/models.py
@@ -1,13 +1,19 @@
 from app import db, login_manager
 import os
+import base64
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from flask_login import UserMixin
+from datetime import datetime, timedelta
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
+
+######### User Class ###############
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,6 +21,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
 
     def __init__(self, username, email, password):
         self.username = username
@@ -23,6 +31,46 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+    def to_dict(self):
+        user_dict = {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email
+        }
+        return user_dict
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update_user(self, data):
+        for field in data:
+            if field in {'username', 'email', 'password'}:
+                if field == 'password':
+                    setattr(self, field, generate_password_hash(data[field]))
+                else:
+                    setattr(self, field, data[field])
+        db.session.commit()
+
+    def get_token(self, expires_in=300):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        db.session.commit()
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+        db.session.commit()
+
+
+
+############## Post Class ##################
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -35,3 +83,24 @@ class Post(db.Model):
         self.title = title
         self.content = content
         self.user_id = user_id
+
+    def to_dict(self):
+        post_dict = {
+            'id': self.id,
+            'title': self.title,
+            'content': self.content,
+            'date_created': self.date_created,
+            'user_id': self.user_id
+
+        }
+        return post_dict
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update_post(self, data):
+        for field in data:
+            if field in {'title', 'content'}:
+                setattr(self, field, data[field])
+        db.session.commit()
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from app import app, db, mail
-from flask import render_template, url_for, redirect, flash
+from flask import render_template, url_for, redirect, flash, jsonify, request
 from flask_mail import Message
 from app.forms import LoginForm, UserInfoForm,  PostForm
 from app.models import Post, User
 from flask_login import login_user, logout_user, current_user, login_required
+from flask_httpauth import HTTPBasicAuth
 
 @app.route('/')
 def index():
@@ -17,6 +18,7 @@ def favs():
     title= 'Five Favorite Countries'
     countries= ['Thailand', 'Spain', 'India', 'New Zealand', 'Nepal']
     return render_template('favs.html', title=title, countries=countries)
+
 
 @app.route('/todo')
 def todo():
@@ -52,6 +54,7 @@ def register():
 
     return render_template('register.html', form=register_form)
 
+
 @app.route('/login', methods =['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -77,11 +80,11 @@ def login():
 
     return render_template('login.html', login_form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
-
 
 
 @app.route('/createpost', methods=['GET', 'POST'])
@@ -100,10 +103,12 @@ def createpost():
         return redirect(url_for('index'))
     return render_template('createpost.html', form=form)
 
+
 @app.route('/myaccount')
 @login_required
 def my_account():
     return render_template('my_account.html')
+
 
 @app.route('/myposts')
 @login_required
@@ -111,10 +116,12 @@ def my_posts():
     posts = current_user.posts
     return render_template('my_posts.html', posts=posts)
 
+
 @app.route('/posts/<int:post_id>')
 def post_detail(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post_detail.html', post=post)
+
 
 @app.route('/posts/<int:post_id>/update', methods=['GET', 'POST'])
 @login_required
@@ -132,21 +139,149 @@ def post_update(post_id):
         post.content = new_content
         db.session.commit()
         
-        flash(f'The {post.title} has been updated', 'success')
+        flash(f'{post.title} has been updated', 'success')
         return redirect(url_for('post_detail', post_id=post.id))
     return render_template('post_update.html', post=post, form=form)
 
 
 @app.route('/posts/<int:post_id>/delete', methods=['POST'])
 @login_required
-def post(post_id):
+def post_delete(post_id):
     post = Post.query.get_or_404(post_id)
     if post.author != current_user:
-        flash("you cannot delete this post", 'danger')
+        flash("You cannot delete this post", 'danger')
         return redirect(url_for('my_posts'))
 
     db.session.delete(post)
     db.session.commit()
 
-    flash("you post has been deleted", 'primary')
+    flash("Your post has been deleted", 'success')
     return redirect(url_for('my_posts'))
+
+
+
+
+################    Start of JSON/API     #######################
+
+
+@app.route('/api/users')
+def get_users():
+    """
+    [GET] /api/users - Returns all users
+    """
+    users = User.query.all()
+    return jsonify([u.to_dict() for u in users])
+
+
+@app.route('/api/users/<id>')
+def get_user(id):
+    """
+    [GET] /api/users/<user_id> - Returns 1 user
+    """
+    user = User.query.get_or_404(id)
+    return jsonify(user.to_dict())
+
+
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    data = request.json
+    for field in ['username', 'email', 'password']:
+        if field not in data:
+            return jsonify({'error': f'You are missing the {field} field'}), 400
+
+    # Grab data from request body     
+    username = data['username']
+    email = data['email']
+    password = data['password']
+
+    #Check if existing user
+    existing_user = User.query.filter_by(username=username).all()
+    if existing_user:
+        return jsonify({'error': f'The username {username} is already in use. Please try again.'}), 400
+
+    #Create new user
+    new_user = User(username, email, password)
+    new_user.save()
+
+
+    return jsonify(new_user.to_dict())
+
+
+@app.route('/api/users/<id>', methods=['PUT'])
+def update_user(id):
+    user = User.query.get_or_404(id)
+    data = request.json
+    user.update_user(data)
+    return jsonify(user.to_dict())
+
+
+@app.route('/api/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    pass
+
+
+
+# basic_auth = HTTPBasicAuth()
+
+# @basic_auth.verify_password
+# def verify(username, password):
+#     user = User.query.filter_by(username=username).first()
+#     if user and user.check_password(password):
+#         return user
+
+
+# @app.route('/api/token', methods=['POST'])
+# @basic_auth.login_required
+# def get_token():
+#     token = basic_auth.current_user().get_token()
+#     return jsonify({'token': token})
+
+
+
+
+@app.route('/api/posts')
+def get_posts():
+    """
+    [GET] /api/posts - Returns all posts
+    """
+    posts = Post.query.all()
+    return jsonify([p.to_dict() for p in posts])
+
+
+@app.route('/api/posts/<id>')
+def get_post(id):
+    """
+    [GET] /api/posts/<user_id> - Returns 1 post
+    """
+    post = Post.query.get_or_404(id)
+    return jsonify(post.to_dict())
+
+
+@app.route('/api/posts', methods=['POST'])
+def create_post():
+    data = request.json
+    for field in ['title', 'content', 'user_id']:
+        if field not in data:
+            return jsonify({'error': f'You are missing the {field} field'}), 400
+     
+    title = data['title']
+    content = data['content']
+    user_id = data['user_id']
+
+    new_post = Post(title, content, user_id)
+    new_post.save()
+
+    return jsonify(new_post.to_dict())
+
+
+@app.route('/api/posts/<id>', methods=['PUT'])
+def update_post(id):
+    post = Post.query.get_or_404(id)
+    data = request.json
+    post.update_post(data)
+    return jsonify(post.to_dict())
+
+
+@app.route('/api/posts/<id>', methods=['DELETE'])
+def delete_post(id):
+    pass
